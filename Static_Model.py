a9e17a719cabde3ddc0baaf17c9d626032067869
lucas-baier <32632225+lucas-baier@users.noreply.github.com>
2019-11-25 18:40:31 +0100

    Change file for classification

--- Static_Model.py
+++ Static_Model.py
@@ -15,13 +15,13 @@ from XGBoostModel import XGBoostModel
 print('Data loading started: ', datetime.now())
 start_time = time.time()
 
-# data = joblib.load("data_ORD_date_class.joblib")
+data = joblib.load("data_ORD_date_class.joblib")
 
 #
 #
 #
 # Local processing
-data = joblib.load("sample_data_ORD_date_class.joblib")
+# data = joblib.load("sample_data_ORD_date_class.joblib")
 
 print('Duration Loading: ', (time.time() - start_time))
 
